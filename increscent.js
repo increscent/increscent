1cbe223944fd63b5eccb524e122b9b3756fa2322
Robert Williams <robert@increscent.org>
2019-02-19 20:15:28 +0000

    added ping

--- increscent.js
+++ increscent.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded());
 // mandelbrot (pull from github)
 app.use('/mandelbrot', express.static(__dirname + '/../mandelbrot/'));
 app.get('/mandelbrot', function(req, res) {
-  res.redirect('/mandelbrot/mandelbrot.html');
+    res.redirect('/mandelbrot/mandelbrot.html');
 });
 
 // initialize app
@@ -35,39 +35,46 @@ ui.init_pages();
 
 // main site
 //app.get('/', function(req, res) {
-//	ui.load_page(res, 'robert');
+//    ui.load_page(res, 'robert');
 //});
 // John's math
 app.get('/math', function(req, res) {
-	ui.load_page(res, 'math');
+    ui.load_page(res, 'math');
 });
 // stocks
 app.get('/stocks', function(req, res) {
-	ui.load_page(res, 'stocks');
+    ui.load_page(res, 'stocks');
 });
-
-
+ping_hosts = {};
+// ping
+app.get('/ping/:hostname', function(req, res) {
+    res.send(ping_hosts[req.params.hostname]);
+});
+app.post('/ping/:hostname', function(req, res) {
+    ping_hosts[req.params.hostname] = req.headers['x-forwarded-for']; 
+    res.send();
+});
 
 // ********** stocks ********** //
 app.post('/stock/*', function(req, res) {
-	var symbol = req.url.replace('/stock/', '');
-	var url = 'http://www.marketwatch.com/investing/stock/' + symbol;
-	http.get(url, function (response) {
-		response.on('data', function (body) {
-			var body_string = body.toString();
-			if (body_string.indexOf('<p class="data bgLast">') !== -1) {
-				res.send(body.toString().match('<p class="data bgLast">(.*?)<\/p>')[1]);
-			}
-		});
-	});
+    var symbol = req.url.replace('/stock/', '');
+    var url = 'http://www.marketwatch.com/investing/stock/' + symbol;
+    http.get(url, function (response) {
+        response.on('data', function (body) {
+            var body_string = body.toString();
+            if (body_string.indexOf('<p class="data bgLast">') !== -1) {
+                res.send(body.toString().match('<p class="data bgLast">(.*?)<\/p>')[1]);
+            }
+        });
+    });
 });
 
 // ********** app ********** //
 
 // empty cached pages
 app.get('/refresh', function(req, res) {
-	ui.empty_page_cache();
-	res.send('Page cache has been emptied');
+    ui.empty_page_cache();
+    res.send('Page cache has been emptied');
 });
 
 // setup port
