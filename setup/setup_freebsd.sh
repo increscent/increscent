f2e8fbb873f22bbef74453425b6cf8d4fbf8513c
Robert Williams <robert@increscent.org>
2020-12-25 14:35:16 -0600

    switched to static nginx hosting

--- setup/setup_freebsd.sh
+++ setup/setup_freebsd.sh
@@ -4,12 +4,6 @@
 
 cd /home/robert/increscent
 
-npm install
-
-cp ./setup/increscent_service /usr/local/etc/rc.d/increscent
-
-printf '\nincrescent_enable="YES"\n' >> /etc/rc.conf
-
 mkdir -p /usr/local/etc/nginx/sites-enabled
 
 cp ./setup/increscent_nginx /usr/local/etc/nginx/sites-enabled/increscent
