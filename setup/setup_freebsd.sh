8e40235d0a5d5092e654da6f55197f3bbe15ee5e
Robert Williams <robert@increscent.org>
2020-12-25 20:40:08 +0000

    updated setup

--- setup/setup_freebsd.sh
+++ setup/setup_freebsd.sh
@@ -4,6 +4,10 @@
 
 cd /home/robert/increscent
 
+rustc liza.rs
+
+./generate.sh
+
 mkdir -p /usr/local/etc/nginx/sites-enabled
 
 cp ./setup/increscent_nginx /usr/local/etc/nginx/sites-enabled/increscent
